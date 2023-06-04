174838aaae429630b627539705ca1e8d11e89dde
Harsh <goyalh4164@gmail.com>
2023-06-04 22:07:22 +0530

    using context in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { Context } from '../main'
 
 const Header = () => {
+  
+  const {isAuthenticated,setIsAuthenticated} = useContext(Context);
+  console.log(isAuthenticated)
+  console.log(setIsAuthenticated)
   return (
     <div>
       <nav>
@@ -9,8 +14,13 @@ const Header = () => {
             <h2>ThoughtFlow</h2>
             <div>
                 <Link to={"/"}>Home</Link>
-                <Link to={"/login"}>Login</Link>
-                <Link to={"/register"}>Register</Link>
+                {
+                  isAuthenticated ? <Link to={"/logout"}>Logout</Link> : <Link to={"/login"}>Login</Link>
+                }
+                {
+                  (!isAuthenticated) ? <Link to={"/register"}>Register</Link> : ""
+                }
+                
             </div>
         </div>
       </nav>
