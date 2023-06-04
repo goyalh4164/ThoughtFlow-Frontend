018a1a35351f860635963fc4c6b8456a32d5b457
Harsh <goyalh4164@gmail.com>
2023-06-04 17:57:36 +0530

    basic template setup for calling api

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerHandler = (e) => {
+    e.preventDefault(); // to prevent refreshing of the pafe
+    console.log(name,email,password)
+  };
   return (
     <div>
       Register
       <section>
-        <form action="">
-          <input type="text" placeholder="name" />
-          <input type="text" placeholder="email" />
-          <input type="password" placeholder="password" />
+        <form onSubmit={registerHandler}>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="name"
+            required
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="email"
+            required
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="password"
+            required
+          />
           <button type="submit">Register</button>
           <h4>Or</h4>
           <Link to="/login">Login</Link>
