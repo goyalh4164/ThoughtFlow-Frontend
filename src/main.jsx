775c757b64673366615d18014330e01215b8060f
Harsh <goyalh4164@gmail.com>
2023-06-04 21:50:27 +0530

    created the context to use it in all the components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createContext } from "react";
 
-export const server = "http://localhost:4000/api/v1"
+export const server = "http://localhost:4000/api/v1";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const Context = createContext({ isAuthenticated: false });
+
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  return (
+    <Context.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppWrapper />
+  </React.StrictMode>
+);
