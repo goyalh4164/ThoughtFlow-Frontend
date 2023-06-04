9d8742ab229fe7d1b52aaed2468876d665311181
Harsh <goyalh4164@gmail.com>
2023-06-04 16:57:48 +0530

    basic form structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div>
       Login
+      <section>
+        <form action="">
+          <input type="text" placeholder='email'/>
+          <input type="password" placeholder='password'/>
+          <button type='submit'>Login</button>
+          <h4>Or</h4>
+          <Link to="/register">Register</Link>
+        </form>
+      </section>
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
     <div>
       Register
+      <section>
+        <form action="">
+          <input type="text" placeholder="name" />
+          <input type="text" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button type="submit">Register</button>
+          <h4>Or</h4>
+          <Link to="/login">Login</Link>
+        </form>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
