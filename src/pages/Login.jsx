08a64a3114bf0181be8754f0e8539697b92b9191
Harsh <goyalh4164@gmail.com>
2023-06-04 21:43:43 +0530

    connected the login component to the backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,52 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import axios from "axios"
+import { server } from "../main";
+import toast from "react-hot-toast"
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler =async (e) => {
+    e.preventDefault(); // to prevent refreshing of the pafe
+    try{
+      console.log(email,password)
+    const {data} =await axios.post(`${server}/users/login`,{
+      email,password
+    },{
+      headers:{
+        "Content-Type":"application/json"
+      },
+      withCredentials:true,
+    });
+    toast.success(data.message);
+    }
+    catch(error){
+      toast.error("Some error")
+      console.log(error)
+    }
+  };
+
   return (
     <div>
       Login
       <section>
-        <form action="">
-          <input type="text" placeholder='email'/>
-          <input type="password" placeholder='password'/>
+        <form onSubmit={loginHandler}>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="email"
+            required
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="password"
+            required
+          />
           <button type='submit'>Login</button>
           <h4>Or</h4>
           <Link to="/register">Register</Link>
@@ -16,6 +54,6 @@ const Login = () => {
       </section>
     </div>
   )
-}
+};
 
 export default Login
