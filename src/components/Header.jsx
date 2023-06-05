4b8de8ce64025ff377b35ca2c8729f48efea709d
Harsh <goyalh4164@gmail.com>
2023-06-05 23:59:08 +0530

    added the logout api setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,33 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { Context } from '../main'
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
+import { server } from '../main';
 
 const Header = () => {
   
   const {isAuthenticated,setIsAuthenticated} = useContext(Context);
   console.log(isAuthenticated)
   console.log(setIsAuthenticated)
+
+  const logoutHandler =async (e) => {
+    try{
+      const {data} =await axios.post(`${server}/users/logout`,
+      {
+      withCredentials:true,
+    }
+    );
+    toast.success(data.message);
+    setIsAuthenticated(false);
+    <Navigate to="/login"/>
+    }
+    catch(error){
+      toast.error("Some error")
+      console.log(error)
+      setIsAuthenticated(true);
+    }
+  };
   return (
     <div>
       <nav>
@@ -15,7 +36,7 @@ const Header = () => {
             <div>
                 <Link to={"/"}>Home</Link>
                 {
-                  isAuthenticated ? <Link to={"/logout"}>Logout</Link> : <Link to={"/login"}>Login</Link>
+                  isAuthenticated ? <button onClick={logoutHandler}>Logout</button> : <Link to={"/login"}>Login</Link>
                 }
                 {
                   (!isAuthenticated) ? <Link to={"/register"}>Register</Link> : ""
